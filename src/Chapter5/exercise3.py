3f1de564ab2b8aec90c827e04ae330c6915ca212
group8BSE1 <2020bse@std.must.ac.ug>
2021-04-22 17:02:57 +0300

    added new global variables and a function stock

--- src/Chapter5/exercise3.py
+++ src/Chapter5/exercise3.py
@@ -6,6 +6,17 @@ one_dollar = 0
 five_dollar = 0
 #Global variables of stock coins
 
+g_money_toPay = 0
+g_cents_toPayConverted = 0
+
+#denminations counter global
+g_nickles_count = 0
+g_dimes_count = 0
+g_quarters_count = 0
+g_ones_count = 0
+g_five_count = 0 
+#denminations counter global
+
 #Function to print the Menu
 def menu_message():
     print('MENU FOR DEPOSITS')
@@ -23,6 +34,22 @@ def stock_contains(n_value, d_value, q_value, o_value, f_value):
     print(f'{q_value} -- quarter')
     print(f'{o_value} -- one dollar bill')
     print(f'{f_value} -- five dollar bill')
+
+#function to update the stock of coins 
+def updateStock(upDate_nickles, upDate_dimes, upDate_quarters, upDate_oneDollar, upDate_fiveDollar):
+    #using global keyword to change a global variable
+    global nickels
+    global dimes
+    global quarters
+    global one_dollar
+    global five_dollar
+
+    #assigning the stock new values
+    nickels = nickels + upDate_nickles
+    dimes = dimes + upDate_dimes
+    quarters = quarters + upDate_quarters
+    one_dollar = one_dollar + upDate_oneDollar
+    five_dollar = five_dollar + upDate_fiveDollar
 
 
 print('WELCOME TO THE VENDING CHANGE MACHINE!\n')
