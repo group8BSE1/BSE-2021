274d0456e97e5653fd10cdfa384632981d3aa19b
group8BSE1 <2020bse052@std.must.ac.ug>
2021-04-23 16:26:40 +0300

    Added functions till i get the balance

--- src/Chapter5/exercise3.py
+++ src/Chapter5/exercise3.py
@@ -35,6 +35,20 @@ def stock_contains(n_value, d_value, q_value, o_value, f_value):
     print(f'{o_value} -- one dollar bill')
     print(f'{f_value} -- five dollar bill')
 
+def testUserInput():
+    moneyToPay = float(input("Enter the purchase price (xx.xx) or `q' to quit: "))
+    status = ''
+    dollar = moneyToPay // 1
+    cents = moneyToPay % 1
+    finalCents = round(cents,2)
+    centsToFull_digit = finalCents * 100
+    totalCents = (dollar * 100) + round(centsToFull_digit)
+    if(totalCents % 5) == 0 and totalCents >= 0:
+        status = 'YES'
+    else:
+        status = 'NO'
+    return status, moneyToPay, totalCents
+
 #function to update the stock of coins 
 def updateStock(upDate_nickles, upDate_dimes, upDate_quarters, upDate_oneDollar, upDate_fiveDollar):
     #using global keyword to change a global variable
@@ -52,7 +66,71 @@ def updateStock(upDate_nickles, upDate_dimes, upDate_quarters, upDate_oneDollar,
     one_dollar = one_dollar + upDate_oneDollar
     five_dollar = five_dollar + upDate_fiveDollar
 
+def moneyUserInput(value_toBePaid):
+    global g_nickles_count
+    global g_dimes_count
+    global g_quarters_count
+    global g_ones_count
+    global g_five_count
+    
+    #counter for coins put (denominations)
+    nickles_count = 0
+    dimes_count = 0
+    quarters_count = 0
+    ones_count = 0
+    five_count= 0 
+    #counter for coins put
 
-print('WELCOME TO THE VENDING CHANGE MACHINE!\n')
-print('CURRENT STOCK CONTAINS>\n')
-stock_contains(nickels, dimes, quarters, one_dollar, five_dollar)
+    cash_put = 0
+    strValueCent = 0
+    while cash_put < value_toBePaid:
+        print(f'Payment due:  dollars and  cents')
+        money = input('Indicate your deposit: ')
+        if money == 'n':
+            strValueCent = 5
+            nickles_count += 1
+        elif money == 'd':
+            strValueCent = 10
+            dimes_count += 1
+        elif money == 'q':
+            strValueCent = 25
+            quarters_count += 1
+        elif money == 'o':
+            strValueCent = 100
+            ones_count += 1
+        elif money == 'f':
+            strValueCent = 500
+            five_count += 1
+        elif money == 'c':
+            break
+        else:
+            pass
+
+        cash_put = cash_put + strValueCent
+
+    g_nickles_count = nickles_count
+    g_dimes_count = dimes_count
+    g_quarters_count = quarters_count
+    g_ones_count = ones_count
+    g_five_count = five_count
+    
+    balance = cash_put - value_toBePaid
+    return balance
+
+def main():
+    while True:
+        inStatus, money_toPay, total_ofCents = testUserInput()
+        global g_cents_toPayConverted 
+        global g_money_toPay 
+        
+        if inStatus == 'YES':
+            g_money_toPay = money_toPay
+            g_cents_toPayConverted = total_ofCents
+
+            menu_message()
+            balance_toGive = moneyUserInput(total_ofCents)
+            print(balance_toGive)
+        else:
+            print('Illegal price, Must be a non-negative multiple of 5 cents.\n')
+        
+main()
