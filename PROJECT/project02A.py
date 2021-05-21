616a57150ce38e8e10f74678324c210984499993
group8BSE1 <2020bse052@std.must.ac.ug>
2021-05-21 20:25:02 +0300

    final piece of project 02

--- PROJECT/project02A.py
+++ PROJECT/project02A.py
@@ -1,17 +1,33 @@
+yearInput=input('Please enter a year: ')
+listOutput = []
 year=input('Enter year:')
 l = []
 
+
 try:
-    with open('measles.txt', 'r') as f:
-        for lines in f:
-            line = lines.split()
+    with open('measles.txt', 'r') as fileRead:
+        for lines in fileRead:
+         with open('measles.txt', 'r') as f:
+            for lines in f:
+             line = lines.split()
             if line:
+                if yearInput in ["ALL", 'all', '']: #If User asks for all the years
+                    listOutput.append(lines)
+                elif line[4].startswith(yearInput): #If the user enters a specific year
+                    listOutput.append(lines)
                 if year in ["ALL", 'all', '']:
                     l.append(lines)
                 elif line[4].startswith(year):
                     l.append(lines)
 
 except IOError:
+     print('Could not read file, File doesnt exist!!')
+
+if listOutput:
+    file = input("Enter name of file in which data will be written: ")
+    with open(file,'w') as fileRead:
+        for lines in listOutput:
+            fileRead.write(lines)
     print('Could not read file')
 if l:
     file=input("Enter name of file in which data will be written: ")
